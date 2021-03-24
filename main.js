21a621e5f3e4fe8c055d614789684da612e1e41e
UTComputerGraphics <utcomputergraphics@gmail.com>
2021-03-24 15:51:28 -0400

    Remove rotation
    Do it later

--- main.js
+++ main.js
@@ -114,8 +114,6 @@ function onWindowResize() {
 
 function animate() {
 	requestAnimationFrame( animate );
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.02;
 
 	renderer.render( scene, camera );
 }
